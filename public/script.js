d8998ad6cff6e6638758f1ea3e4ddedc1b8ea327
Arman <arman1apollon@gmail.com>
2024-06-03 10:56:35 +0100

    html code editor updates the renderer when playground responds

--- public/script.js
+++ public/script.js
@@ -78,6 +78,7 @@ async function compileAndRunCode() {
       const formattedHTML = html_beautify(data.Events[0].Message);
       htmlCodeEditor.setValue(formattedHTML);
       document.getElementById("render").innerHTML = formattedHTML;
+      htmlCodeEditor.VirtualRenderer().updateFull(true);
       runButton.classList.toggle("loading");
     });
 
